8ddc9aac7aa816eb27a6a97ce19aa2c21039818c
jean-christophe bauduin <bauduinjc@gmail.com>
2019-04-01 11:58:31 +0200

    build

---
+++ clientWebApp/client-app/build/precache-manifest.c218ec46831e1493cbdc0f0a6de079ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "d8f93a77d2cd7237d718",
+    "url": "/static/js/main.a37292eb.chunk.js"
+  },
+  {
+    "revision": "00044b51b4914f7992f3",
+    "url": "/static/js/2.f4c3d140.chunk.js"
+  },
+  {
+    "revision": "d8f93a77d2cd7237d718",
+    "url": "/static/css/main.8a889210.chunk.css"
+  },
+  {
+    "revision": "ee60add369da3c9a87b7f3f271bfbf4b",
+    "url": "/index.html"
+  }
+];
