7d24966553a5ae175b6f9ab70996125e8564c026
David Lefebvre <davidlfbvr19@gmail.com>
2019-04-29 13:40:42 +0200

    add switch button

--- clientWebApp/client-app/src/App.js
+++ clientWebApp/client-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import io from 'socket.io-client';
-import {Navbar,NavItem,Col,Row} from "react-materialize"
+import {Navbar,NavItem,Col,Row,Switch} from "react-materialize"
 import logo from './logo.svg';
 import './App.css';
 import BNOA from './Components/BNOA'
@@ -14,24 +14,39 @@ import BMPH from './Components/BMP/BMPH'
 var io = require('socket.io-client');
 const socket = io.connect('http://localhost:3000', {reconnect: true});
 
-class App extends Component {
+export default class App extends Component {
   constructor(props){
     super(props);
+    this.onSwitch = this.onSwitch.bind(this)
     this.state = {
-
+      lora: false
     }
   }
   componentDidMount(){
-
+    
+  }
+  onSwitch(e){
+    this.setState({
+      lora: !this.state.lora
+    })
   }
   render() {
     return (
       <div>
       <Navbar className="teal accent-3" brand='Velocity' right>
       </Navbar>
+      <div className="switch">
+    <label>
+      Off
+      <input value={this.state.lora} onChange={this.onSwitch} type="checkbox"/>
+      <span className="lever"></span>
+      On
+    </label>
+  </div>
+
       <Row>
         <Col s={12} m={6}>
-          <BNOA socket={socket}/>
+          <BNOA socket={socket} lora={this.state.lora}/>
         </Col>
         <Col s={12} m={6}>
           <PITOT socket={socket}/>
@@ -57,4 +72,3 @@ class App extends Component {
   }
 }
 
-export default App;
--- clientWebApp/client-app/src/Components/BNOA.js
+++ clientWebApp/client-app/src/Components/BNOA.js
@@ -5,6 +5,7 @@ import {Col,Row,Card} from "react-materialize"
 class BNOA extends Component {
   constructor(props){
     super(props);
+    this.connectOnSocket = this.connectOnSocket.bind(this)
     this.state = {
       x: 0,
       y: 0,
@@ -12,16 +13,29 @@ class BNOA extends Component {
       XPoints: Array(1).fill(null),
       YPoints: Array(1).fill(null),
       ZPoints: Array(1).fill(null),
-      data: []
+      data: [],
+      lora: props.lora
     }
+  }
+  componentWillReceiveProps(nextProps) {
+    // You don't have to do this check first, but it can help prevent an unneeded render
+    this.setState({ lora: nextProps.lora });
+
+    //this.connectOnSocket()
   }
   componentDidMount(){
     this.setState({data: getData([])})
 
   }
-  componentWillMount(){
+  connectOnSocket(){
     var that = this
-    this.props.socket.on('BNOA', ({x,y,z})=>{
+    var txt = ""
+    if(this.state.lora){
+      txt = "LORABNOA"
+    }else{
+      txt = "BNOA"
+    }
+    this.props.socket.on(txt, ({x,y,z})=>{
       that.setState({
         x: x,y: y,z: z,
         XPoints: [...that.state.XPoints.slice(-10),x], //that.state.Xpoint.push(x)
@@ -30,12 +44,16 @@ class BNOA extends Component {
         data:getData(that.state.XPoints,that.state.YPoints,that.state.ZPoints)
       });
     });
+  }
+  componentWillMount(){
+    this.connectOnSocket()
 
   }
   render() {
     return (
       <div className="component">
-          <Card className ='grey lighten-3' textClassName='grey-text' title='BNO A'>
+          <p>Capting on {this.state.lora ? "Lora antenna" : "xbee antenna"}</p>
+          <Card className ='grey lighten-3' textClassName='grey-text' title='Acceleration'>
           <Line data={this.state.data}/>
           <p className="center">{this.state.x}, {this.state.y},{this.state.z}</p>
           </Card>
