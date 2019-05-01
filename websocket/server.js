687042965bf984be0335c08350793f3a8fb30d5b
David Lefebvre <davidlfbvr19@gmail.com>
2019-05-01 18:32:18 +0200

    add switch button

--- websocket/server.js
+++ websocket/server.js
@@ -25,21 +25,12 @@ io.on('connection', client => {
         client.broadcast.emit("FSR",data)
     });
 
-
-
-<<<<<<< HEAD
     client.on('LORA-BNOA', (data) => { 
         console.log(data);
         client.broadcast.emit("LORA-BNOA",data)
     }); 
     client.on('LORA-PITOT', (data) => {
-=======
-    client.on('LORABNOA', (data) => {
-        console.log(data);
-        client.broadcast.emit("LORABNOA",data)
-    });
-    client.on('LORAPITOT', (data) => {
->>>>>>> 286086c28370f1e8917205285a02cb43c8b0a60a
+
         console.log(data);
         client.broadcast.emit("LORA-PITOT",data)
     });
